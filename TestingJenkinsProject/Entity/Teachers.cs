5e6d61899e2af7a451f2adcf03994f807105d07c
Manul99 <114052195+Manul99@users.noreply.github.com>
2025-01-16 21:23:21 +0530

    First commit

---
+++ TestingJenkinsProject/Entity/Teachers.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingJenkinsProject.Entity
+{
+    public class Teachers
+    {
+        public int TecherID { get; set; }
+        public string? TeacherName { get; set; }
+        public int mobile { get; set; }
+    }
+}
